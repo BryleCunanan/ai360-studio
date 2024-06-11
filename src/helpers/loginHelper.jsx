d047267ef503c53ff64d8894da4a3a7eb97ecd88
Bryle Paulo Cunanan <brylepaulocunanan@gmail.com>
2024-06-11 10:35:39 +0800

    changed clearing of localstorage from token to all

--- src/helpers/loginHelper.jsx
+++ src/helpers/loginHelper.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export function deleteLogin(error, navigate) {
-  if (error?.response?.data === "Invalid token") {
+  if (error?.response?.data === "Invalid token" || error == "logout") {
     console.log("Logging out");
     delete axios.defaults.headers.common["Authorization"];
-    localStorage.removeItem("token");
+    localStorage.clear();
   }
 
   if (!axios.defaults.headers.common["Authorization"]) {
